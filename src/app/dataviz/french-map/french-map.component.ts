bfae0b1a184f432f33e390ee81636ee80c41693f
Jérémy GUEGAN <jeremy.guegan@gmail.com>
2019-07-25 15:17:08 +0200

    changement couleur

--- src/app/dataviz/french-map/french-map.component.ts
+++ src/app/dataviz/french-map/french-map.component.ts
@@ -12,6 +12,9 @@ import { ViewEncapsulation } from '@angular/core';
 export class FrenchMapComponent implements OnInit, OnChanges {
 
   @Input() json: any;
+  @Input() hue = '240';
+  @Input() contrast = 100;
+  @Input() saturation = '100';
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.json.currentValue) {
@@ -30,11 +33,12 @@ export class FrenchMapComponent implements OnInit, OnChanges {
     const height = 700;
     const formatNumber = d3.format('s');
     const populationBins = this.getBins(this.json);
-    const colorBins = populationBins.map(n => `hsl(240, 30%, ${100 - n * 100 / 3000000}%)`);
+    const colorBins = populationBins.
+    map(n => `hsl(${this.hue}, ${this.saturation}%, ${100 - n * this.contrast / this.getMax(this.json)}%)`);
 
     const color = d3.scaleThreshold<number, string>()
       .domain(populationBins)
-      .range(['#deebf7', '#c6dbef', '#9ecae1', '#6baed6', '#4292c6', '#2171b5', '#08519c', '#08306b']);
+      .range(colorBins);
 
     const x = d3.scaleLinear()
       .domain([0, this.getMax(this.json)])
