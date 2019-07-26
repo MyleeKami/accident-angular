a2e3360028aaa524a4e9823eb30de6f11e8ba721
Jérémy GUEGAN <jeremy.guegan@gmail.com>
2019-07-26 10:57:21 +0200

    requete depuis le service OK

--- src/app/dataviz/wikidata-image-list/wikidata-image-list.component.ts
+++ src/app/dataviz/wikidata-image-list/wikidata-image-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WikidataService } from 'src/app/wikidata.service';
 
 @Component({
   selector: 'app-wikidata-image-list',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wikidata-image-list.component.scss']
 })
 export class WikidataImageListComponent implements OnInit {
+  result: any[];
 
-  constructor() { }
+  constructor(private wd: WikidataService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    const sparql = `
+    SELECT ?h ?hLabel ?dead ?image
+    WHERE
+    {
+      ?h wdt:P31 wd:Q5.
+      ?h wdt:P570 ?dead.
+      ?h wdt:P509 wd:Q9687.
+      ?h wdt:P27 wd:Q142.
+      Optional{ ?h wdt:P18 ?image. }.
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+    }
+      ORDER by desc(?dead)`;
+    this.result = await this.wd.request(sparql);
   }
 
 }
---
+++ src/app/wikidata.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WikidataService {
+  endpoint = 'https://query.wikidata.org/sparql';
+  constructor() { }
+
+  async request(sparql: string): Promise<any[]> {
+
+    const fullUrl = this.endpoint + '?query=' + encodeURIComponent(sparql);
+    const headers = { Accept: 'application/sparql-results+json' };
+    const body = await fetch(fullUrl, { headers });
+    const json = await body.json();
+    return json;
+  }
+}
