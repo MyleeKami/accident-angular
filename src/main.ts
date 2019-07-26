2ca8680a7b7c4d3c808aba228a6b1749cd6ab319
Jérémy GUEGAN <jeremy.guegan@gmail.com>
2019-07-26 12:01:10 +0200

    ok

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+  console.log = () => {}; //desactive les sorties consoles pour la prod
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
