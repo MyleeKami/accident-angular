471bd4428efff6cd7bf618f9f8c72956cb49ef2a
Jérémy GUEGAN <jeremy.guegan@gmail.com>
2019-07-26 09:58:22 +0200

    ajout composant de dataviz wikidata

---
+++ src/app/dataviz/wikidata-image-list/wikidata-image-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wikidata-image-list',
+  templateUrl: './wikidata-image-list.component.html',
+  styleUrls: ['./wikidata-image-list.component.scss']
+})
+export class WikidataImageListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
